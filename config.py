bd0b5061504fe31b233f2e4e97e6fa7706523a2b
Magnus Pernemark <magnus.pernemark@tietoevry.com>
2025-10-24 08:19:55 +0200

    settings

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///ticketbroker.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Email configuration
